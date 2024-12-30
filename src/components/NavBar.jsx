914be019ff31e3e169009672fba744077d2c5af4
liptwo <liptwo0@gmail.com>
2024-12-30 13:54:16 +0700

    fix icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,17 @@
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import SettingsIcon from '@mui/icons-material/Settings';
+import SearchIcon from '@mui/icons-material/Search';
+
 import React from 'react'
 
 const NavBar = () => {
   return (
     <div className='bg-black w-full h-[10vh] grid grid-cols-3 gap-4 '>
-        <div className='col-span-2'>
+        <div className='col-span-2 text-white flex flex-row items-center'>
             <input className='h-12 font-medium text-white m-4 pl-4 w-[70%] rounded-full bg-search ' type="text" placeholder='Search for a song'/>
-            <i className="fa fa-search sm:hidden md:hidden relative left-[-3rem] text-2xl text-white l-[-20px]" aria-hidden="true"></i>
+            <div className='relative left-[-4rem]'>
+                 <SearchIcon sx={{fontSize:30}} />
+            </div>
         </div>
         <div className='flex text-white col-span-1 items-center justify-end'>
             <div className='pr-4 '>
@@ -17,11 +23,11 @@ const NavBar = () => {
                     <span className='tems-center bg-slate-700 px-1 mt-1 ml-1 rounded text-sm font-nomarl text-white'>Premium</span>
                 </div>
             </div>
-            <div className='w-[3rem] h-[3rem] bg-gray-800 rounded-full flex mx-4 hover:bg-slate-400 active:bg-slate-500'>
-                <i className="fa fa-heart m-auto scale-125"></i>
+            <div className='w-[3rem] h-[3rem] bg-gray-800 rounded-full flex justify-center items-center mx-4 hover:bg-slate-400 active:bg-slate-500'>
+                <FavoriteIcon sx={{fontSize:30,}}/>
             </div>
-            <div className='w-[3rem] h-[3rem] bg-gray-800 rounded-full flex mr-4 hover:bg-slate-400 active:bg-slate-500'>
-                <i className="fa fa-gear m-auto scale-125"></i>
+            <div className='w-[3rem] h-[3rem] bg-gray-800 rounded-full flex justify-center items-center mr-4 hover:bg-slate-400 active:bg-slate-500'>
+                <SettingsIcon sx={{fontSize:30,}} />
             </div>
         </div>
 

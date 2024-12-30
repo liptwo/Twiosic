6c21ff9f49e8a4dd238e47bca0a381e46b5c7a16
liptwo <liptwo0@gmail.com>
2024-12-30 23:05:57 +0700

    responesive hero

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,79 +1,94 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-import {Swiper, SwiperSlide} from 'swiper/react';
-import PlayCircleIcon from '@mui/icons-material/PlayCircle';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from "swiper/react";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import {EffectCoverflow , Pagination, Navigation} from 'swiper/modules';
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 
-import slide_image_1 from '../assets/images/thumnail1.webp'
-import slide_image_2 from '../assets/images/thumnail2.webp'
-import slide_image_3 from '../assets/images/thumnail3.webp'
-import slide_image_4 from '../assets/images/thumnail4.webp'
-import slide_image_5 from '../assets/images/thumnail5.webp'
+import slide_image_1 from "../assets/images/thumnail1.webp";
+import slide_image_2 from "../assets/images/thumnail2.webp";
+import slide_image_3 from "../assets/images/thumnail3.webp";
+import slide_image_4 from "../assets/images/thumnail4.webp";
+import slide_image_5 from "../assets/images/thumnail5.webp";
 const slide_array = [
-  slide_image_1, slide_image_2, slide_image_3, slide_image_4,slide_image_5 ,
-]
+  slide_image_1,
+  slide_image_2,
+  slide_image_3,
+  slide_image_4,
+  slide_image_5,
+];
 
 const HeroBanner = () => {
-
-  const handleHidden = ()=>{
-    const needhidden = document.getElementsByClassName("swiper-slide-active need-hidden");
+  const handleHidden = () => {
+    const needhidden = document.getElementsByClassName(
+      "swiper-slide-active need-hidden"
+    );
     needhidden.toggleClass("hidden");
-  }
+  };
   // useEffect(() => {
   //   handleHidden();
   // }, [12])
-  
 
   return (
-    <div className=' flex justify-center box-border items-center h-[40vh] px-16 py-4 mx-0 my-auto bg-black overflow-hidden scroll-smooth'>
-        <Swiper
-        effect={'coverflow'}
-         grabCursor={true}
-         centeredSlides={true}
-         loop={true}
-         slidesPerView={'auto'}
-         coverflowEffect={{
-           rotate: 0,
-           stretch: 100,
-           depth:90,
-           modifier: 2.5,
-           slideShadows: true,
-         }}
-         autoplay={{
-          delay:300,
-          disableOnInteraction:false,
-         }}
-         pagination={{ el: '.swiper-pagination', clickable: true }}
+    <div className=" flex justify-center box-border items-center h-[40vh] px-16 py-4 mx-auto my-auto bg-black overflow-hidden scroll-smooth">
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 100,
+          depth: 90,
+          modifier: 2.5,
+          slideShadows: true,
+        }}
+        autoplay={{
+          delay: 300,
+          disableOnInteraction: false,
+        }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
         modules={[EffectCoverflow, Pagination, Navigation]}
         className="swiper_container"
-         >
-          {slide_array.map((slide_image)=>(
-              <SwiperSlide>
-                <div className=''>
-                  <div>
-                    <img className='w-[18rem] shadow-inner' src={slide_image} alt="slide_image" />
-                  </div>
-                  
-                </div>
-                <div className='text-white need-hidden justify-between w-[15rem] hidden flex-row relative left-[2rem] top-[-5rem] items-center'>
-                    <div>
-                      <h1 className=''>EChoes of Midnight</h1>
-                      <span className=''>Jon Hickman</span>
-                    </div>
-                    <div  className='ml-8 '> 
-                      <PlayCircleIcon sx={{fontSize: 50,}}></PlayCircleIcon>
-                    </div>
-                  </div>
-              </SwiperSlide>
-          ))}
-        </Swiper>
+      >
+        {slide_array.map((slide_image) => (
+          <SwiperSlide>
+            <div className="">
+              <div>
+                <img
+                  className="w-[20rem] shadow-inner"
+                  src={slide_image}
+                  alt="slide_image"
+                />
+              </div>
+            </div>
+            <div className="text-white need-hidden justify-between w-[15rem] hidden flex-row relative left-[2rem] top-[-5rem] items-center">
+              <div>
+                <h1 className="title">EChoes of Midnight</h1>
+                <span className="title">Jon Hickman</span>
+              </div>
+              <div className="button ml-8 ">
+                <PlayCircleIcon
+                  sx={{
+                    fontSize:{
+                    xs: 20, // kích thước icon khi màn hình nhỏ (dưới 600px)
+                    sm: 30, // kích thước icon khi màn hình từ 600px đến 960px
+                    md: 40, // kích thước icon khi màn hình từ 960px đến 1280px
+                    lg: 50,
+                  }}}
+                ></PlayCircleIcon>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
--- src/components/home/menu.jsx
+++ src/components/home/menu.jsx
@@ -1,3 +1,4 @@
+import { ArrowRightAlt, ArrowRightSharp, ChevronLeftOutlined, LogoutSharp } from '@mui/icons-material'
 import aGirl from '../../images/aGirl.png'
 
 import React from 'react'
@@ -8,67 +9,71 @@ const Menu = () => {
         <div className='block text-orange-400 mx-auto text-center text-3xl p-4 font-semibold'>
           Twiosic 
         </div>
-
-        <div className='mx-6'>
-          <div className='block p-3 hover:cursor-pointer hover:bg-orange-500 hover:text-white rounded'>
-            <i class="fa-solid fa-house pr-2"></i>
-            Home
-          </div>
-
-          <div className='block p-3 hover:cursor-pointer hover:bg-orange-500 hover:text-white rounded'>
-            <i class="fa-solid fa-list pr-2"></i>
-            Categories
-          </div>
-
-          <div className='block p-3 hover:cursor-pointer hover:bg-orange-500 hover:text-white rounded'>
-            <i class="fa-regular fa-user pr-2"></i>
-            Aritists
-          </div>
-
-          <div className='block p-3'>
-            <div className='hover:text-white cursor-pointer'>
-              <i class="fa-solid fa-music pr-2"></i>
-              Playlists
-              <i class="fa-solid fa-chevron-up pl-11"></i>
+        <div className='flex flex-col justify-between h-[80vh]'>
+        <div>
+          <div className='mx-6'>
+            <div className='block p-3 hover:cursor-pointer hover:bg-orange-500 hover:text-white rounded'>
+              <i class="fa-solid fa-house pr-2"></i>
+              Home
             </div>
 
-            <div className='list-item list-none ml-7 mt-2 overflow-scroll scrollbar-hide max-h-[300px] hover:bg-gray-900 rounded'>
-              <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'>
-                <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
-                Playlist 1
-              </li>
-              <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'>
-                <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
-                Playlist 2
-              </li>
-              <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'> 
-                <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
-                Playlist 3
-              </li>
-              <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'> 
-                <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
-                Playlist 4
-              </li>
-              <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'> 
-                <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
-                Playlist 5
-              </li>
-              <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'> 
-                <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
-                Playlist 6
-              </li>
-              <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'> 
-                <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
-                Playlist 7
-              </li>
+            <div className='block p-3 hover:cursor-pointer hover:bg-orange-500 hover:text-white rounded'>
+              <i class="fa-solid fa-list pr-2"></i>
+              Categories
             </div>
-          </div>
 
-          <div className='block p-2 hover:cursor-pointer hover:bg-orange-500 hover:text-white rounded'>
-            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+            <div className='block p-3 hover:cursor-pointer hover:bg-orange-500 hover:text-white rounded'>
+              <i class="fa-regular fa-user pr-2"></i>
+              Aritists
+            </div>
+
+            <div className='block p-3'>
+              <div className='hover:text-white cursor-pointer'>
+                <i class="fa-solid fa-music pr-2"></i>
+                Playlists
+                <i class="fa-solid fa-chevron-up pl-11"></i>
+                {/* <ChevronLeftOutlined/> */}
+              </div>
+
+              <div className='list-item list-none ml-7 mt-2 overflow-scroll scrollbar-hide max-h-[300px] hover:bg-gray-900 rounded'>
+                <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'>
+                  <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
+                  Playlist 1
+                </li>
+                <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'>
+                  <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
+                  Playlist 2
+                </li>
+                <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'> 
+                  <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
+                  Playlist 3
+                </li>
+                <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'> 
+                  <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
+                  Playlist 4
+                </li>
+                <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'> 
+                  <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
+                  Playlist 5
+                </li>
+                <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'> 
+                  <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
+                  Playlist 6
+                </li>
+                <li className='flex items-center hover:bg-orange-500 hover:text-white rounded p-2 cursor-pointer'> 
+                  <img src={aGirl} className='bg-cover w-[40px] rounded-lg mr-2'></img>
+                  Playlist 7
+                </li>
+              </div>
+          </div>
+          </div> 
+          </div>
+          <div className='relative  p-1 m-3 left-[20px] bottom-[20px] hover:cursor-pointer  hover:text-white rounded'>
+            {/* <i class="fa-solid fa-arrow-right-from-bracket"></i> */}
+            <LogoutSharp/> 
             Log Out
           </div>
-
+           
         </div>
     </div>
   )
